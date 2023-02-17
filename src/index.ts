fd7801f2dbf6aa44e6f3226c0f3ddf6f48857a10
datldh <datldh21@gmail.com>
2023-02-17 14:47:44 +0700

    module-exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-export { EncryptFieldsData } from "./cryptData/encryptFieldsData";
-export { DecryptFieldsData } from "./cryptData/decryptFieldsData";
-export { addWatermarkToImage } from "./watermark/addWatermarkToImage";
+import { DecryptFieldsData } from "./cryptData/decryptFieldsData";
+import { EncryptFieldsData } from "./cryptData/encryptFieldsData";
+import { addWatermarkToImage } from "./watermark/addWatermarkToImage";
+
+module.exports = { EncryptFieldsData, DecryptFieldsData, addWatermarkToImage };
