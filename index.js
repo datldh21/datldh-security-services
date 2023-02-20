469f26f318704f86a04b1ad56275a61e262b5c78
datldh <datldh21@gmail.com>
2023-02-20 14:43:34 +0700

    update version 0

---
+++ index.js
@@ -0,0 +1,9 @@
+import { EncryptFieldsData } from "./src/cryptData/encryptFieldsData";
+import { DecryptFieldsData } from "./src/cryptData/decryptFieldsData";
+import { addWatermarkToImage } from "./src/watermark/addWatermarkToImage";
+
+module.exports = {
+    EncryptFieldsData,
+    DecryptFieldsData,
+    addWatermarkToImage,
+};
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { EncryptFieldsData } from "./cryptData/encryptFieldsData";
-import { DecryptFieldsData } from "./cryptData/decryptFieldsData";
-import { addWatermarkToImage } from "./watermark/addWatermarkToImage";
-
-module.exports = {
-    EncryptFieldsData,
-    DecryptFieldsData,
-    addWatermarkToImage,
-};
